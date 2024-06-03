164722e0edaf847cdf7db17076bff225cf680412
Sonal <skumari@dovercorp.com>
2024-06-03 10:52:28 +0530

    completed till video 48

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -26,7 +26,55 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name","Display Order cannot exactly match the name.");
+            }
+
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invalid value");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
             _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Find only works for the primary key which is id in this case
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+
+            //FirstOrDefault can work for any value from the model
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //Works when you need to do some calculation before FirstOrDefault
+            //Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb); 
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            _db.Categories.Update(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -11,9 +11,11 @@ namespace BulkyWeb.Models
         public int Id { get; set; } //primary key of the table
 
         [Required]  //makes Name not nullable
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage = "Display Order must be between 1-100.")]
         public int DisplayOrder { get; set; }
 
     }
